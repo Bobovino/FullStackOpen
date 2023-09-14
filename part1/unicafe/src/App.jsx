989f08676d7971aec5ac5b3ecf72c31fbdc346da
Bobovino <rodrigomartineztabernero97@gmail.com>
2023-09-14 12:24:25 +0200

    1.6

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -6,11 +6,35 @@ const App = () => {
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
+  const incrementGood=() =>{
+    return setGood(good+1)
+  }
+
+  const incrementNeutral=() =>{
+    return setNeutral(neutral+1)
+  }
+
+  const incrementBad=() =>{
+    return setBad(bad+1)
+  }
   return (
+    <>
+    <h1>
+      Give feedback
+    </h1>  
     <div>
-      code here
-    </div>  
-  );
-};
+      <button onClick={incrementGood}>Good</button>
+      <button onClick={incrementNeutral}>Neutral</button>
+      <button onClick={incrementBad}>Bad</button>
+    </div>
+    <h1>
+      Statistics
+    </h1> 
+    <p>good, {good}</p>
+    <p>neutral, {neutral}</p>
+    <p>bad, {bad}</p>
+    </>
+  )
+}
 
 export default App;
