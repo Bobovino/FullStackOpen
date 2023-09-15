0178ec88cb2ffe01e7de5fffbb30ddc623075b5d
Bobovino <rodrigomartineztabernero97@gmail.com>
2023-09-15 13:44:02 +0200

    2.11

--- part2/La guía telefónica/src/App.jsx
+++ part2/La guía telefónica/src/App.jsx
@@ -1,18 +1,29 @@
-import { useState } from 'react';
+import {useEffect, useState } from 'react';
+import axios from 'axios'
 import Filter from './Components/Filter.jsx'
 import Form from './Components/Form.jsx'
 import Persons from './Components/Persons.jsx'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
+  /*   { name: 'Arto Hellas', number: '040-123456' },
     { name: 'Ada Lovelace', number: '39-44-5323523' },
     { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
+    { name: 'Mary Poppendieck', number: '39-23-6423122' } */
   ]);
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [searchedName,setSearchedName]=useState('')
+
+  useEffect(() => {
+    console.log('effect')
+    axios
+      .get('http://localhost:5000/persons/')
+      .then(response => {
+        console.log('promise fulfilled')
+        setPersons(response.data)
+      })
+  }, [])
   
   return (
     <>
