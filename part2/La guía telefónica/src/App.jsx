8170d705c73fb2ada6f1c7db21d523325b550abb
Bobovino <rodrigomartineztabernero97@gmail.com>
2023-09-15 19:46:42 +0200

    2.15

--- part2/La guía telefónica/src/App.jsx
+++ part2/La guía telefónica/src/App.jsx
@@ -5,15 +5,7 @@ import Form from './Components/Form.jsx'
 import Persons from './Components/Persons.jsx'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-  /*   { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' } */
-  ]);
-  const [newName, setNewName] = useState('')
-  const [newNumber, setNewNumber] = useState('')
-  const [searchedName,setSearchedName]=useState('')
+  const [persons, setPersons] = useState([]);
 
   useEffect(() => {
     console.log('effect')
@@ -28,11 +20,8 @@ const App = () => {
   return (
     <>
       <h2>Phonebook</h2>
-      <Filter searchedName={searchedName} setSearchedName={setSearchedName} persons={persons}/>
-        
-      <Form persons={persons} newName={newName} newNumber={newNumber} 
-      setPersons={setPersons} setNewName={setNewName} setNewNumber={setNewNumber} />
-      
+      <Filter persons={persons} />
+      <Form persons={persons} setPersons={setPersons}  />
       <Persons persons={persons}/>
     </>
   );
